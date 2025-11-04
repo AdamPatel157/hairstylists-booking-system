422f9087ae386869335f0182c53e06464589853d
AdamPatel157 <adampatel157@outlook.com>
2025-11-04 23:03:14 +0000

    Fixed Account Creation validation not functioning correctly by separating field validation into individual functions and only proceeding if each function returns True.

--- website/user_redirection.py
+++ website/user_redirection.py
@@ -6,6 +6,90 @@ from flask import Blueprint, render_template, request, flash, redirect
 
 user_redirection = Blueprint("auth", __name__)
 
+# Registration Validation Functions
+# If validation test for field passes, returns True
+# If validation test for field fails, returns False and displays error message
+# All functions must return True for registration to be successful
+
+def validateName(name, namePos):
+    lowercaseEnglishAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    if len(name) < 2:
+        flash(f"{namePos} Name must be greater than 1 character long.", category="Error")
+        return False
+    elif any(character.lower() not in lowercaseEnglishAlphabet for character in name):
+        flash(f"{namePos} Name can only contain English alphabet characters.", category="Error")
+        return False
+    else:
+        return True
+
+def validateEmail(emailAddress):
+    if len(emailAddress) < 4:
+        flash("Length of Email must be greater than 3 characters long.", category="Error")
+        return False
+    elif '@' not in list(emailAddress):
+        flash("Email must contain '@' symbol.", category="Error")
+        return False
+    elif '.' not in list(emailAddress):
+        flash("Email must contain '.' symbol.", category="Error")
+        return False
+    else:
+        return True
+
+def validatePhoneNumber(phoneNum):
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    if len(phoneNum) != 11:
+        flash("Phone Numbers must be exactly 11 characters long.", category="Error")
+        return False
+    else:
+        for character in phoneNum:
+            if character not in numbers:
+                flash("Phone Numbers can only contain integer numbers 0-9.", category="Error")
+                return False
+        if phoneNum[0] != '0':
+            flash("Phone Numbers must begin with the number '0'.", category="Error")
+            return False
+        else:
+            return True
+
+def validatePassword(createPassword, confirmPassword):
+    passwordPunctuation = ['?', '!', '£', '%', '^', '&', '*', '(', ')', '/', '#']
+    lowercaseEnglishAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    if len(createPassword) < 8:
+        flash("Passwords must be at least 8 characters in length.", category="Error")
+        return False
+
+    elif not any(character.lower() in lowercaseEnglishAlphabet for character in createPassword):
+        flash("Passwords must contain at least one English alphabet character.", category="Error")
+        return False
+
+    elif not any(character in numbers for character in createPassword):
+        flash("Passwords must contain at least one integer number 0-9.", category="Error")
+        return False
+
+    elif not any(character in passwordPunctuation for character in createPassword):
+        flash("Passwords must contain at least one punctuation character from ['?', '!', '£', '%', '^', '&', '*', '(', ')', '/', '#'].", category="Error")
+        return False
+
+    elif createPassword == createPassword.lower():  # If there are no uppercase letters
+        flash("Passwords must contain at least one uppercase letter.", category="Error")
+        return False
+
+    elif createPassword == createPassword.upper():  # If there are no lowercase letters
+        flash("Passwords must contain at least one lowercase letter.", category="Error")
+        return False
+
+    elif createPassword != confirmPassword:  # If the confirmation password does not equal the created password
+        flash("Passwords do not match.", category="Error")
+        return False
+
+    else:
+        return True
+
+# Processes webpage HTTP requests
+
 @user_redirection.route("/login", methods=["GET", "POST"])
 def login():
     data = request.form # Accesses the form attribute data from the incoming POST Request
@@ -24,79 +108,17 @@ def register():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        # Registration Form Input Validation:
+        # Passes form inputs into registration validation procedures
+        # The procedures return True if passes validation and False if it fails
+        firstNameIsValidated = validateName(firstName, "First")
+        middleNameIsValidated = validateName(middleName, "Middle")
+        lastNameIsValidated = validateName(lastName, "Last")
+        emailIsValidated = validateEmail(email)
+        phoneNumberIsValidated = validatePhoneNumber(phoneNumber)
+        passwordIsValidated = validatePassword(password1, password2)
 
-        # Name Fields Validation:
-
-        lowercaseEnglishAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                                    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-        passwordPunctuation = ['?', '!', '£', '%', '^', '&', '*', '(', ')', '/', '#']
-
-        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-        if len(firstName) < 2:
-            flash("First Name must be greater than 1 character long.", category="Error")
-        elif any(character.lower() not in lowercaseEnglishAlphabet for character in firstName):
-            flash("First Name can only contain English alphabet characters.", category="Error")
-
-        elif len(lastName) < 2: # Ensures the length of last names are not 0 or 1 character long
-            flash("Last Name must be greater than 1 character long.", category="Error")
-        elif any(character.lower() not in lowercaseEnglishAlphabet for character in firstName):
-            flash("Last Name can only contain English alphabet characters.", category="Error")
-            # Ensures last names only contain English alphabet characters
-
-        # No Length Validation for middleName as middleName is an optional field so can have 0 characters if left blank
-        elif len(middleName) > 0:
-            for character in middleName:
-                if character.lower() not in lowercaseEnglishAlphabet: # Ensures middle names only contain English alphabet characters
-                    flash("Middle Name can only contain English alphabet characters.", category="Error")
-
-        # Email Validation:
-
-        elif len(email) < 4:
-            flash("Length of Email must be greater than 3 characters long.", category="Error")
-
-        elif "@" not in email or "." not in email:
-            flash("Email must contain '@' and '.' symbols.", category="Error")
-
-        # Phone Number Validation:
-
-        elif len(phoneNumber) > 0:
-            if len(phoneNumber) != 11:
-                flash("Phone Numbers must be exactly 11 characters long.", category="Error")
-            else:
-                for character in phoneNumber:
-                    if character not in numbers:
-                        flash("Phone Numbers can only contain integer numbers 0-9.", category="Error")
-                if phoneNumber[0] != '0':
-                    flash("Phone Numbers must begin with the number '0'.", category="Error")
-
-        # Password Validation:
-
-        elif len(password1) < 8:
-            flash("Passwords must be at least 8 characters in length.", category="Error")
-
-        elif not any(character.lower() in lowercaseEnglishAlphabet for character in password1):
-            flash("Passwords must contain at least one English alphabet character.", category="Error")
-
-        elif not any(character in numbers for character in password1):
-            flash("Passwords must contain at least one integer number 0-9.", category="Error")
-
-        elif not any(character in passwordPunctuation for character in password1):
-            flash("Passwords must contain at least one punctuation character from "
-                  "['?', '!', '£', '%', '^', '&', '*', '(', ')', '/', '#'].", category="Error")
-
-        elif password1 == password1.lower(): # If there are no uppercase letters
-            flash("Passwords must contain at least one uppercase letter.", category="Error")
-
-        elif password1 == password1.upper(): # If there are no lowercase letters
-            flash("Passwords must contain at least one lowercase letter.", category="Error")
-
-        elif password1 != password2: # If the confirmation password does not equal the created password
-            flash("Passwords do not match.", category="Error")
-
-        else:
+        # If all validation check procedures are true, proceeds
+        if firstNameIsValidated and middleNameIsValidated and lastNameIsValidated and emailIsValidated and phoneNumberIsValidated and passwordIsValidated:
             flash("Account Successfully Created.", category = "Success")
             return redirect("/login")
 
@@ -104,4 +126,5 @@ def register():
 
 @user_redirection.route("/logout")
 def logout():
-    return "<p>You have successfully logged out</p>"
+    flash("You have logged out and been returned to the home page.", category="Success")
+    return redirect("/")
